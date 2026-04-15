a1fb163f7652dee16e3cb883e641d59ec4ea4edf
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2026-04-15 19:32:30 +0200

    Pop Up Menu C++ Class Final Touches
    - To decouple responsibilities, the game logic will be handled in Inv_InventoryGrid, the Inv_ItemPopUp will broadcast newly created delegates and close itself only by RemoveFromParent
    - The delegates are not multicast as only one listener is required which is Inv_InventoryGrid, there for when broadcasting the delegates ExecuteIfBound() is used
    - Added public setters and getters for the grid index, added a public getter for the size box
    - Overridden NativeOnMouseLeave() to ensure the menu close itself once the mouse moves away
    - To allow the pop up menu to be context sensitive, the unwanted buttons will be collapsed, added two public utility functions CollapseSplitButton() to hide all split components (Button - Slider - Text) CollapseConsumeButton() to hide the consume button
    - Added a new utility function SetSliderParams(), to set the parameters of the slider (Max Value - Min Value = 1 - Current Value)
    - Implemented OnSliderValueChanged() to ensure the text block gets updated dynamically once the slider is moved even without splitting

--- Plugins/InventoryPlugin/Source/InventoryPlugin/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
+++ Plugins/InventoryPlugin/Source/InventoryPlugin/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
@@ -2,7 +2,9 @@
 
 #include "Widgets/ItemPopUp/Inv_ItemPopUp.h"
 #include "Components/Button.h"
+#include "Components/SizeBox.h"
 #include "Components/Slider.h"
+#include "Components/TextBlock.h"
 
 void UInv_ItemPopUp::NativeOnInitialized()
 {
@@ -15,18 +17,67 @@ void UInv_ItemPopUp::NativeOnInitialized()
 	
 }
 
+void UInv_ItemPopUp::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseLeave(InMouseEvent);
+	this->RemoveFromParent();
+}
+
+int32 UInv_ItemPopUp::GetSplitAmount() const
+{
+	return FMath::Floor(Slider_Split->GetValue());
+}
+
 void UInv_ItemPopUp::OnSplitButtonClicked()
 {
+	if (OnSplit.ExecuteIfBound(GetSplitAmount(), GridIndex))
+	{
+		this->RemoveFromParent();
+	}
 }
 
 void UInv_ItemPopUp::OnDropButtonClicked()
 {
+	if (OnDrop.ExecuteIfBound(GridIndex))
+	{
+		this->RemoveFromParent();
+	}
 }
 
 void UInv_ItemPopUp::OnConsumeButtonClicked()
 {
+	if (OnConsume.ExecuteIfBound(GridIndex))
+	{
+		this->RemoveFromParent();
+	}
 }
 
 void UInv_ItemPopUp::OnSliderValueChanged(float Value)
 {
+	Text_SplitAmount->SetText(FText::AsNumber(FMath::Floor(Value)));
+}
+
+void UInv_ItemPopUp::CollapseSplitButton() const
+{
+	Button_Split->SetVisibility(ESlateVisibility::Collapsed);
+	Slider_Split->SetVisibility(ESlateVisibility::Collapsed);
+	Text_SplitAmount->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UInv_ItemPopUp::CollapseConsumeButton() const
+{
+	Button_Consume->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UInv_ItemPopUp::SetSliderParams(const float Max, const float CurrentValue) const
+{
+	Slider_Split->SetMaxValue(Max);
+	Slider_Split->SetMinValue(1);
+	Slider_Split->SetValue(CurrentValue);
+	Text_SplitAmount->SetText(FText::AsNumber(FMath::Floor(CurrentValue)));
+}
+
+FVector2D UInv_ItemPopUp::GetBoxSize() const
+{
+	return FVector2D(SizeBox_Root->GetWidthOverride(), SizeBox_Root->GetHeightOverride());
 }
--- Plugins/InventoryPlugin/Source/InventoryPlugin/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
+++ Plugins/InventoryPlugin/Source/InventoryPlugin/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -10,6 +10,11 @@ class USizeBox;
 class UTextBlock;
 class USlider;
 class UButton;
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FPopMenuSplit, int32, SplitAmount, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopMenuDrop, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuConsume, int32, Index);
+
 /**
  * The item popup widget shows up when right-clicking on an item
  * in the inventory grid.
@@ -18,10 +23,26 @@ UCLASS()
 class INVENTORYPLUGIN_API UInv_ItemPopUp : public UUserWidget
 {
 	GENERATED_BODY()
-	
+		
 public:
-	virtual void NativeOnInitialized() override;
 	
+	FPopMenuSplit OnSplit;
+	FPopMenuDrop OnDrop;
+	FPopUpMenuConsume OnConsume;
+	
+	int32 GetSplitAmount() const;
+	void CollapseSplitButton() const;
+	void CollapseConsumeButton() const;
+	void SetSliderParams(const float Max, const float CurrentValue) const;
+	FVector2D GetBoxSize() const;
+	void SetGridIndex(const int32 Index) { this->GridIndex = Index;}
+	int32 GetGridIndex() const {return this->GridIndex;}
+	
+protected:
+	
+	virtual void NativeOnInitialized() override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+
 private:
 	
 	UFUNCTION()
@@ -35,6 +56,8 @@ private:
 	
 	UFUNCTION()
 	void OnSliderValueChanged(float Value);
+	
+	
 	
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UButton> Button_Split;
@@ -54,7 +77,6 @@ private:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<USizeBox> SizeBox_Root;
 	
-	
-	
-	
+	int32 GridIndex{INDEX_NONE}; /*To allow the pop-up menu to know which inventory item it belongs to*/
+
 };
